c3cdc811f6b1df5934fe894670f0e6f2309530c5
JB Christy <jbchrsty@stanford.edu>
2022-03-03 17:33:03 -0800

    remove debug info, actually implement the filter

--- login-redirect.php
+++ login-redirect.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Login Redirect
- * Description: Some plugins set login redirects to $_SERVER['SERVER_NAME'], which fails on Pantheon servers. Use $_SERVER['HTTP_HOST] instead.
+ * Description: Some plugins set login redirects to $_SERVER['SERVER_NAME'], which fails on Pantheon servers. Use site_url() instead.
  * Author: JB Christy
  * Author URI: https://www.stanford.edu/site
  * Text Domain: stanvord
@@ -11,14 +11,21 @@
 
 namespace Stanford\Login_Redirect;
 
-function fix_hostname( string $redirect_to, string $requested_redirect_to, WP_User $user ) {
-  if ( function_exists( '\Stanford\ConsoleLog\console_log' ) ) {
-    \Stanford\ConsoleLog\console_log( [
-      '$redirect_to'           => $redirect_to,
-      '$requested_redirect_to' => $requested_redirect_to,
-      '$user'                  => $user,
-    ] );
-  }
-  return $redirect_to;
+/**
+ * The LH Private Content Login plugin sometimes gives us a wonky host name in the url
+ * we're being redirected to. In a Local by Flywheel dev environment, it includes Local's
+ * port, which doesn't work. On Pantheon servers, it uses $_SERVER['SERVER_NAME'],
+ * e.g. appserver-440fae55-nginx-74fc8f611e5f4fc29105c69b83f33b42:11559. This function
+ * replaces the host:port with site_url.
+ * Invoked via the login_redirect filter.
+ *
+ * @param string $redirect_to - where we want to end up
+ * @param string $requested_redirect_to - also where we want to end up??? - ignored
+ * @param \WP_User | \WP_Error $user - WP_Error if logged out - ignored
+ * @return string url to redirect to
+ */
+function fix_hostname( $redirect_to, $requested_redirect_to, $user ) {
+  $url = site_url( parse_url( $redirect_to, PHP_URL_PATH ) );
+  return $url;
 }
-add_filter( 'login_redirct', 'Stanford\Login_Redirect\fix_hostname', 99, 3 );
+add_filter( 'login_redirect', 'Stanford\Login_Redirect\fix_hostname', 99, 3 );
